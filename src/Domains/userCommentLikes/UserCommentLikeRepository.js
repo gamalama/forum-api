a3aa051ba04433b43357d2d82750e14d18bccb16
Fakhry Linux <fakhry.linux@gmail.com>
2024-08-12 16:35:05 +0800

    Add UserCommentLikeRepository interface

---
+++ src/Domains/userCommentLikes/UserCommentLikeRepository.js
@@ -0,0 +1,15 @@
+class UserCommentLikeRepository {
+  async verifyUserCommentLikeIsExist() {
+    throw new Error('USER_COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async addUserCommentLike() {
+    throw new Error('USER_COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteUserCommentLike() {
+    throw new Error('USER_COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = UserCommentLikeRepository;
